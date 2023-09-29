d6845c9e6124bd20df2c520ac36b2cea5f2035e4
Larissa de Sousa <larissa.sousa.lds@gmail.com>
2023-09-29 17:19:25 +0100

    dev da chamada de api

--- src/pages/alimentos/Alimentos.js
+++ src/pages/alimentos/Alimentos.js
@@ -1,5 +1,22 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 const Alimentos = () => {
-	return <div></div>;
+	const [nutri, setNutri] = useState([]);
+
+	const getNutri = async (query = 'beans') => {
+		try {
+			const response = await axios.get(`https://api.edamam.com/auto-complete?app_id=0b650757&app_key=5ce57803a5c4cbd68ebcee1104cc491f&q=${query}`);
+
+			const data = response.data;
+			setNutri(data);
+		} catch (error) {
+			console.error('Erro na busca de dados');
+		}
+	};
+
+	useEffect(() => {
+		getNutri();
+	}, []);
 };
 
 export default Alimentos;
